d617be1f434e382fcad38d77d760af0823fd6c2e
Lars Kanis <lars@greiz-reinsdorf.de>
2017-08-13 13:57:16 +0200

    a

--- release.rb
+++ release.rb
@@ -1,10 +1,12 @@
-module Ri2GemHelper
+module RubyInstaller
+module Build
+class Release
   def hfile
     "CHANGELOG.md"
   end
 
   def headline_regex
-    '(?<product>[\w]+)-(?<version>\d+\.\d+\.\d+-[\d\w]+)([^\w]+)(?<date>[2Y][0Y][0-9Y][0-9Y]-[0-1M][0-9M]-[0-3D][0-9D])([ \w]*)$'
+    '(?<pre>[^\w]*)(?<release>[\w]+-\d+\.\d+\.\d+-[\d\w]+)(?<sp1>[^\w]+)(?<date>[2Y][0Y][0-9Y][0-9Y]-[0-1M][0-9M]-[0-3D][0-9D])(?<sp2>[ \w]*)$'
   end
 
   def reldate
@@ -12,45 +14,36 @@ module Ri2GemHelper
   end
 
   def release_text
-    m = File.read(hfile).match(/#{headline_regex}(?<annotation>.*?)#{headline_regex}/m) || raise("Unable to find release notes in #{hfile}")
+    m = File.read(hfile).match(/(?<annotation>#{headline_regex}.*?)#{headline_regex}/m) || raise("Unable to find release notes in #{hfile}")
     m[:annotation]
-  end
-
-  def headline
-    m = File.read(hfile).match(/#{headline_regex}/) || raise("Unable to find release header in #{hfile}")
-    m[0]
   end
 
   def release_name
     m = File.read(hfile).match(/#{headline_regex}/)
-    "#{m[:product]}-#{m[:version]}"
+    "#{m[:release]}"
   end
 
   def update_history
     hin = File.read(hfile)
     hout = hin.sub(/#{headline_regex}/) do
-      raise "#{hfile} isn't up-to-date for version #{version}" unless $2==version.to_s
+      raise "#{hfile} isn't up-to-date for version #{release_name}" unless $2==release_name.to_s
       $1 + $2 + $3 + reldate + $5
     end
     if hout != hin
-      Bundler.ui.confirm "Updating #{hfile} for release."
+      $stderr.puts "Updating #{hfile} for release."
       File.write(hfile, hout)
       Rake::FileUtilsExt.sh "git", "commit", hfile, "-m", "Update release date in #{hfile}"
     end
   end
 
   def tag_version
-    Bundler.ui.confirm "Tag release with annotation:"
+    rel = release_name.downcase
+    $stderr.puts "Tag release #{rel} with annotation:"
     rt = release_text
-    Bundler.ui.info(rt.gsub(/^/, "    "))
-    IO.popen(["git", "tag", "--file=-", version_tag], "w") do |fd|
+    $stderr.puts(rt.gsub(/^/, "    "))
+    IO.popen(["git", "tag", "--file=-", rel, "--cleanup=whitespace"], "w") do |fd|
       fd.write rt
     end
-    yield if block_given?
-  rescue
-    Bundler.ui.error "Untagging #{version_tag} due to error."
-    sh_with_code "git tag -d #{version_tag}"
-    raise
   end
 
   CONTENT_TYPE_FOR_EXT = {
@@ -59,28 +52,19 @@ module Ri2GemHelper
     ".7z" => "application/zip",
     ".yml" => "application/x-yaml",
   }
-end
 
-if $0==__FILE__
-  include Ri2GemHelper
-
-  case ARGV[0]
-  when "create_release"
-    files = ARGV[1..-1]
-
+  def upload_to_github(tag:, repo:, token: nil, files:)
     require "octokit"
 
-    repo = ENV['DEPLOY_REPO_NAME']
-    tag = ENV['DEPLOY_TAG']
-
-    raise "tag #{tag.inspect} != #{release_name.inspect}" if tag != release_name.downcase
+    headline = IO.popen(["git", "tag", "-l", tag, "--format=%(subject)"], &:read)
+    body = IO.popen(["git", "tag", "-l", tag, "--format=%(body)"], &:read)
 
     client = Octokit::Client.new(access_token: ENV['DEPLOY_TOKEN'])
     $stderr.puts "Create github release #{tag}"
     release = client.create_release(repo, tag,
         target_commitish: "master",
         name: headline,
-        body: release_text,
+        body: body,
         draft: true,
         prerelease: true
     )
@@ -90,7 +74,7 @@ if $0==__FILE__
       client.upload_asset(release.url, fname, content_type: CONTENT_TYPE_FOR_EXT[File.extname(fname)])
       $stderr.puts "OK"
     end
-  else
-    raise "invalid option"
   end
+end
+end
 end
